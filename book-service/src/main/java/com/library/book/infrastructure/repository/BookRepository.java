4136a2ecf899abfc9aebc4d480beca06e5003897
Vu Xuan Tan <tanvx.training@gmail.com>
2025-05-26 00:03:57 +0700

    book-service::FR-BOOK-005: implement publisher API

--- book-service/src/main/java/com/library/book/infrastructure/repository/BookRepository.java
+++ book-service/src/main/java/com/library/book/infrastructure/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.library.book.infrastructure.repository;
 
 import com.library.book.domain.model.Category;
 import com.library.book.domain.model.Book;
+import com.library.book.domain.model.Publisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,6 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Page<Book> findAllByCategories(List<Category> categories, Pageable pageable);
+
+    Page<Book> findAllByPublisher(Publisher publisher, Pageable pageable);
 }
