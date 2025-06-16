26c53962a29fe31ebc1ab2b42895df4c91bc6fda
Vu Xuan Tan <tanvx.training@gmail.com>
2025-06-16 23:37:42 +0700

    book-service::FR-NTF-001: update handler exception

--- user-service/src/main/java/com/library/user/UserServiceApplication.java
+++ user-service/src/main/java/com/library/user/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.library.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.library.user", "com.library.common"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
